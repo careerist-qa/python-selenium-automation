2d0bf8afafee4a2759ab310aef038195d099af32
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-04-16 19:08:16 -0700

    Add first selenium script

---
+++ sample_script.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.google.com/')
+
+# populate search field
+search = driver.find_element(By.NAME, 'q')
+search.clear()
+search.send_keys('Car')
+
+# wait for 4 sec
+sleep(4)
+
+# click search button
+driver.find_element(By.NAME, 'btnK').click()
+
+# verify search results
+assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+print('Test Passed')
+
+driver.quit()
