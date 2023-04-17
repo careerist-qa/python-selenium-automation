75616b008fb3bd4d17d11a0a61c714f236525d86
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-04-16 19:10:06 -0700

    Add Behave structure and sample test

---
+++ features/environment.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def browser_init(context):
+    """
+    :param context: Behave context
+    """
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
+
+    context.driver.maximize_window()
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+
+
+def before_scenario(context, scenario):
+    print('\nStarted scenario: ', scenario.name)
+    browser_init(context)
+
+
+def before_step(context, step):
+    print('\nStarted step: ', step)
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        print('\nStep failed: ', step)
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
---
+++ features/tests/product_search.feature
@@ -0,0 +1,7 @@
+Feature: Test Scenarios for Search functionality
+
+  Scenario: User can search for a product
+    Given Open Google page
+    When Input Car into search field
+    And Click on search icon
+    Then Product results for Car are shown
