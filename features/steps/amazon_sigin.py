9e68c703c016e0327efa47b42c52157db549a4a6
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-05-24 19:53:04 -0700

    Lesson 5

--- features/steps/amazon_sigin.py
+++ features/steps/amazon_sigin.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
+
 
 SIGNIN_HEADER = (By.XPATH, "//h1[@class='a-spacing-small']")
 EMAIL = (By.ID, 'ap_email')
@@ -7,6 +9,9 @@ EMAIL = (By.ID, 'ap_email')
 
 @then('Verify Sign In page opens')
 def verify_signin_opens(context):
+    # Verify URL:
+    context.driver.wait.until(EC.url_contains('https://www.amazon.com/ap/signin'))
+    # Verify header
     actual_text = context.driver.find_element(*SIGNIN_HEADER).text
     assert actual_text == 'Sign in', f'Expected Sign in header but got {actual_text}'
     # Verify email field present
--- sample_script.py
+++ sample_script.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 # get the path to the ChromeDriver executable
@@ -12,6 +14,18 @@ service = Service(driver_path)
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
+# Applied to all Find Element(s) functions
+# 100 ms = 0.1 s
+# Defined once
+# Not modifying the Error / Exception
+driver.implicitly_wait(5)
+
+# start with driver.wait
+# checks for condition to be met every 500 ms (1/2 s)
+# Defined in the spot where we need it
+# always fails with TimeoutException
+driver.wait = WebDriverWait(driver, 5)
+
 # open the url
 driver.get('https://www.google.com/')
 
@@ -21,10 +35,12 @@ search.clear()
 search.send_keys('Car')
 
 # wait for 4 sec
-sleep(4)
+# sleep(4)
+google_search_btn = (By.NAME, 'btnK')
+driver.wait.until(EC.element_to_be_clickable(google_search_btn), message='Google search btn not clickable')
 
 # click search button
-driver.find_element(By.NAME, 'btnK').click()
+driver.find_element(*google_search_btn).click()
 
 # verify search results
 assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
