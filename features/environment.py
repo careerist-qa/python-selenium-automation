9df284294c84e21b8d644179a93e84b3cf3a004d
mosqueailenis <mosquea.ilenis@gmail.com>
2023-07-31 21:45:54 -0400

    update on file for chrome

--- features/environment.py
+++ features/environment.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 def browser_init(context):
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
+
+    service = Service(executable_path='/Users/ilenismosquea/Desktop/HelloWorld/QA/python-selenium-automation/chromedriver')
     context.driver = webdriver.Chrome(service=service)
 
     context.driver.maximize_window()
--- sample_script.py
+++ sample_script.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 from time import sleep
 
-# get the path to the ChromeDriver executable
-driver_path = ChromeDriverManager().install()
+
 
 # create a new Chrome browser instance
-service = Service(driver_path)
+service = Service(executable_path='/Users/ilenismosquea/Desktop/HelloWorld/QA/python-selenium-automation/chromedriver')
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 
@@ -18,7 +17,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Car')
+search.send_keys('Table')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +26,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'car' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'table' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
