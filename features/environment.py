0244748b603905ee6b760d5c8bd94eb3529bafb9
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-09-02 12:08:23 -0700

    Lesson 8

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 
 from app.application import Application
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):  # add scenario_name if you want to use it in Browserstack
     """
     :param context: Behave context
     """
     service = Service(executable_path='/Users/svetlanalevinsohn/careerist/15-python-selenium-automation/chromedriver')
     context.driver = webdriver.Chrome(service=service)
 
-    context.driver.maximize_window()
+    ### OTHER BROWSERS ###
+    # service = Service(executable_path='/Users/svetlanalevinsohn/careerist/15-python-selenium-automation/geckodriver')
+    # context.driver = webdriver.Firefox(service=service)
+    # context.driver = webdriver.Safari()
 
+    ### HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # service = Service(executable_path='/Users/svetlanalevinsohn/careerist/15-python-selenium-automation/chromedriver')
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    #     service=service
+    # )
+
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    # bs_user = ''
+    # bs_key = ''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     'os': 'Windows',
+    #     'osVersion': '10',
+    #     'browserName': 'Firefox',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
+
+
+    context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
 
@@ -23,7 +54,8 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # Pass scenario.name to init() for browserstack config:
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
--- pages/base_page.py
+++ pages/base_page.py
@@ -24,6 +24,28 @@ class Page:
         e = self.driver.find_element(*locator)
         e.send_keys(text)
 
+    def get_current_window(self):
+        return self.driver.current_window_handle
+
+    def get_windows(self):
+        windows = self.driver.window_handles
+        print(windows)
+        return windows
+
+    def switch_to_new_window(self):
+        self.wait.until(EC.new_window_is_opened)
+        all_windows = self.driver.window_handles
+        print(all_windows)
+        print(f'Switching to {all_windows[1]}')
+        self.driver.switch_to.window(all_windows[1])
+
+    def switch_to_window(self, window_id):
+        print(f'Switching to {window_id}')
+        self.driver.switch_to.window(window_id)
+
+    def close_page(self):
+        self.driver.close()
+
     def wait_for_element_clickable(self, *locator):
         self.wait.until(
             EC.element_to_be_clickable(locator),
