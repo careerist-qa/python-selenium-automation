56df72bdbbcffb76bebd97b75d78b6622d64b1ff
Joshua B <joshhbizz@gmail.com>
2023-09-15 18:35:38 -0400

    completed homework 3 required section

--- features/steps/homework_3_steps.py
+++ features/steps/homework_3_steps.py
@@ -17,4 +17,4 @@ def click_orders(context):
 def verify_sign_in(context):
     expected_result = 'Sign In'
     actual_result = context.driver.find_element(By.XPATH, "//h1[contains(text(), 'Sign in')]")
-    assert expected_result == actual_result f'Error expected {expected_result} did not match actual {actual_result}'
+    assert expected_result == actual_result, f'Error expected {expected_result} did not match actual {actual_result}'
