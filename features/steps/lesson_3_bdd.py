961343d7d508e06e26849f5b4c5366d2dc620cae
Jacob Grable <grabledjacob@gmail.com>
2023-09-19 14:00:39 -0700

    updated hw

--- features/steps/lesson_3_bdd.py
+++ features/steps/lesson_3_bdd.py
@@ -1,13 +1,11 @@
-from behave import given, when ,then
-
+from behave import given, when, then
+from selenium.webdriver.common.by import By
 @given('Open Amazon Page')
 def open_amazon(context):
-    context.driver.get('https://amazon.com/')
-
+    context.driver.get('https://www.amazon.com/')
 @when('Click on Returns and Orders')
 def click_returns_orders(context):
     context.driver.find_element(By.ID, "nav-orders").click()
-
 @then('Taken to Sign In page')
 def sign_in_page(context):
     context.driver.find_element(By.ID, "ap-email")
