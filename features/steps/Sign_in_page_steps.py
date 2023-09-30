763f20e4c7ac0301983ec46b9bc4090d995b1f96
Joshua B <joshhbizz@gmail.com>
2023-09-30 06:25:14 -0400

    fix verification error homework_6

--- features/steps/Sign_in_page_steps.py
+++ features/steps/Sign_in_page_steps.py
@@ -5,6 +5,5 @@ from pages.base_page import Page
 
 
 @then('Verify Sign-in page is opened')
-def verify_sign_in(context):
-    context.app.sign_in_page.verify_sign_in()
-    context.app.sign_in_page.verify_partial_url('com/ap/signin?')
+def verify_sign_in_opened(context):
+    context.app.sign_in_page.verify_sign_in_open()
--- pages/sign_in_page.py
+++ pages/sign_in_page.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 
 class SignInPage(Page):
     SIGN_IN = (By.CSS_SELECTOR, "h1.a-spacing-small")
+    EMAIL_INPUT = (By.ID, 'ap_email')
 
-    def verify_sign_in(self):
+    def verify_sign_in_open(self):
        self.verify_text('Sign in', *self.SIGN_IN)
+       self.driver.find_element(*self.EMAIL_INPUT)
 
-    def verify_partial_url(self,expected_part_of_url):
+    def verify_partial_url(self, expected_part_of_url):
         self.verify_partial_text(expected_part_of_url)
