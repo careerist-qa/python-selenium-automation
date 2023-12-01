03edabef81e3e32ac6d6adc1e9a5736391ced12c
Svetlana Levinsohn <svetlana.levinsohn@gmail.com>
2023-11-30 19:47:27 -0800

    Lesson 10

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from support.logger import logger
 
 from app.application import Application
 
@@ -53,16 +54,21 @@ def browser_init(context, scenario_name):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
+    logger.info(f'\nStarted scenario: {scenario.name}')
     browser_init(context, scenario.name)
 
 
 def before_step(context, step):
     print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
+        context.driver.save_screenshot(f'{step}.png')
         print('\nStep failed: ', step)
+        # logger.warn(f'Started step: {step}')
+        logger.warning(f'Started step: {step}')
 
 
 def after_scenario(context, feature):
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from support.logger import logger
 
 
 class Page:
@@ -10,17 +11,22 @@ class Page:
 
     def open_url(self, url):
         self.driver.get(url)
+        logger.info(f'Opening url {url}')
 
     def click(self, *locator):
         self.driver.find_element(*locator).click()
+        logger.info(f'Clicking on {locator}')
 
     def find_element(self, *locator):
+        logger.info(f'Searching for element {locator}')
         return self.driver.find_element(*locator)
 
     def find_elements(self, *locator):
+        logger.info(f'Searching for elements {locator}')
         return self.driver.find_elements(*locator)
 
     def input(self, text, *locator):
+        logger.info(f"Inputting text '{text}' for element {locator}")
         self.driver.find_element(*locator).send_keys(text)
 
     def get_current_window(self):
@@ -36,6 +42,9 @@ class Page:
 
     def switch_to_window(self, window_id):
         self.driver.switch_to.window(window_id)
+
+    def save_screenshot(self, name):
+        self.driver.save_screenshot(f'{name}.png')
 
     def close_page(self):
         self.driver.close()
