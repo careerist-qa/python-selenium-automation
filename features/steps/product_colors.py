78ca8242589aa15e54baae2a3c44282feb1b26a6
Eugene <zaycevea@gmail.com>
2023-12-03 15:18:41 -0500

    hw5

---
+++ features/steps/product_colors.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+
+COLOR_OPTIONS = (By.CSS_SELECTOR, "[class*='ButtonWrapper'] img")
+SELECTED_COLOR = (By.CSS_SELECTOR, "[class*='StyledVariationSelectorImage'] [class*='CellVariationHeaderWrapper']")
+
+@given('Open target product A-88345426 page')
+def open_target(context):
+    context.driver.get('https://www.target.com/p/A-88345426')
+    sleep(6)
+
+
+@then('Verify user can click through colors')
+def click_and_verify_colors(context):
+    expected_colors = ['Brown', 'Oatmeal', 'Gray', 'Black']
+    actual_colors = []
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
+    for color in colors:
+        color.click()
+        selected_color = context.driver.find_element(*SELECTED_COLOR).text.split('\n')[1]
+        actual_colors.append(selected_color)
+
+    assert expected_colors == actual_colors, f'Expected {expected_colors} did not match actual {actual_colors}'
---
+++ features/tests/product_colors.feature
@@ -0,0 +1,5 @@
+Feature: Product colors
+
+  Scenario: User can select colors
+    Given Open target product A-88345426 page
+    Then Verify user can click through colors
