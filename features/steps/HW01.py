8c7c13eeebc86d29d63580074796b089ba103b81
Shaheena11 <Shaheena@Yusras-MBP.lan>
2024-05-17 14:11:25 -0500

    test

---
+++ features/steps/HW01.py
@@ -0,0 +1,35 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.amazon.com/')
+
+driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']")
+abcd = driver.find_element(By.XPATH, "//i[@class='a-icon a-icon-logo']")
+
+driver.find_element(By.XPATH,"//input[@type='email']")
+
+driver.find_element(By.XPATH,"//input[@id='continue']")
+
+
+
+driver.find_element(By.XPATH,"//a[text()='Conditions of Use']")
+
+driver.find_element(By.XPATH,"//a[@href='/gp/help/customer/display.html/ref=ox_signin_privacy?ie=UTF8&amp;nodeId=468496']")
+
+driver.find_element(By.XPATH,"//input[@name='metadata1']")
+
+driver.find_element(By.XPATH,"//a[@id='createAccountSubmit']")
+
---
+++ features/steps/target_search.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+# open the url
+driver.get('https://www.target.com/')
+
+sleep()
