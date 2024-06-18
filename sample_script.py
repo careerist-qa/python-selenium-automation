c267b1bce734b31495d5c2015b6e764b78cc1259
Justin K <sykwakkr@gmail.com>
2024-06-19 05:06:25 +0900

    Update automated search script

--- sample_script.py
+++ sample_script.py
@@ -18,7 +18,7 @@ driver.get('https://www.google.com/')
 # populate search field
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Table')
+search.send_keys('Dog')
 
 # wait for 4 sec
 sleep(4)
@@ -27,7 +27,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify search results
-assert 'table' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'dog' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
