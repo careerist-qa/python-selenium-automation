a61792743c931718eaf904695f0bd0d3c6e50a0c
Justin K <sykwakkr@gmail.com>
2024-07-02 15:07:42 -0700

    Remove an unnessary driver.get line

--- hw3_amazon_script.py
+++ hw3_amazon_script.py
@@ -15,7 +15,6 @@ driver.maximize_window()
 
 # 1. Practice with locators
 # open the url
-# driver.get('https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
 driver.get('https://www.amazon.com/')
 
 # open sign in page
